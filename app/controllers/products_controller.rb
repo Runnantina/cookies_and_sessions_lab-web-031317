499cd68070bc7d1252b7f8980396cfeeba91f8ab
Runnan Zhong <r.tina.zhong@gmail.com>
2017-04-13 16:02:49 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,8 +5,11 @@ class ProductsController < ApplicationController
   end
 
   def add
-    cart << params[:product] # where is :product coming from
-    redirect_to products_path 
+    cart << params[:product]
+    # where is :product coming from?
+    # params is the user input or from URL, therefore :product is coming from the input of the form in view file:
+    # <input type="text" name="product">
+    redirect_to products_path
   end
 
 end
